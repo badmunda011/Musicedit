6feb23b34997462e7fa0ef73e915f59c486d8c8d
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2023-11-05 15:45:25 +0530

    Update welcome.py

--- VipX/plugins/welcome.py
+++ VipX/plugins/welcome.py
@@ -6,22 +6,9 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from VipX import app  
 
 photo = [
-    "https://telegra.ph/file/1b819cfbcb2a2d3c738f6.jpg",
-    "https://telegra.ph/file/3021c823c7f006658682f.jpg",
-    "https://telegra.ph/file/05561f0fbf323e057ab87.jpg",
-    "https://telegra.ph/file/7a6b51ee0077724254ca7.jpg",
-    "https://telegra.ph/file/b3de9e03e5c8737ca897f.jpg",
-    "https://telegra.ph/file/0b6bb91986ef3a143033b.jpg",
-    "https://telegra.ph/file/2b5b66c9a0989afa0779a.jpg",
-    "https://telegra.ph/file/471339bb1901a007c0c2f.jpg",
-    "https://telegra.ph/file/ab7d958d707ef649bc3c3.jpg",
-    "https://telegra.ph/file/4f877f2843f31fcc32242.jpg",
-    "https://telegra.ph/file/ecefaa3e00fb911826673.jpg",
-    "https://telegra.ph/file/2a10683a2166f7cf4940b.jpg",
-    "https://telegra.ph/file/cda35ca740e1229626e48.jpg",
-    "https://telegra.ph/file/9f7186cd3d87199426e03.jpg",
-    "https://telegra.ph/file/7fe39f1baa1a93b9a3f0e.jpg",
-    
+    "https://te.legra.ph/file/eb5b17bf2fdc09f73f6b2.jpg",
+    "https://te.legra.ph/file/d4bf46af570e1d72eabd5.jpg",
+    "https://graph.org/file/40ce67d14a3f5a7136090.jpg",
 ]
 
 
@@ -34,11 +21,6 @@ async def join_watcher(_, message):
             count = await app.get_chat_members_count(chat.id)
 
             msg = (
-                f"**❥"•❥°☆'🦋°🌸
-
-           †   𝐃𝐢𝐥 𝐊𝐢 𝐃𝐡𝐚𝐝𝐤𝐚𝐧 𝐎𝐫 𝐌𝐞𝐫𝐢 '' 𝐒𝐚𝐝𝐚 𝐇𝐚𝐢 𝐖𝐨✭🖤
-      𝐌𝐞𝐫𝐢 𝐏𝐞𝐡𝐥𝐢 𝐎𝐫 𝐀𝐤𝐡𝐫𝐢  🌍𝐖𝐚𝐟𝐚 𝐇𝐚𝐢 𝐖𝐨  
-                     🍃 𝐂𝐡𝐚𝐡𝐚 𝐇𝐚𝐢 𝐔𝐬𝐞 𝐂𝐡𝐚𝐡𝐚𝐭 𝐒𝐞 𝐁𝐚𝐝𝐡 𝐊𝐚𝐫  ✭ 𝐌𝐞𝐫𝐢 𝐂𝐡𝐚𝐡𝐚𝐭 𝐎𝐫 𝐂𝐡𝐚𝐡𝐚𝐭 𝐊𝐢 𝐈𝐦𝐭𝐚𝐡𝐚𝐚 𝐇𝐚𝐢 𝐖𝐨  ✨🌾:**
                 f"**🌷𝐇ᴇʏ {message.from_user.mention} 𝐖ᴇʟᴄᴏᴍᴇ 𝐈ɴ 𝐀 𝐍ᴇᴡ 𝐆ʀᴏᴜᴘ🥳**\n\n"
                 f"**📝𝐂ʜᴀᴛ 𝐍ᴀᴍᴇ:** {message.chat.title}\n➖➖➖➖➖➖➖➖➖➖➖\n"
                 f"**🔐𝐂ʜᴀᴛ 𝐔.𝐍:** @{message.chat.username}\n➖➖➖➖➖➖➖➖➖➖➖\n"

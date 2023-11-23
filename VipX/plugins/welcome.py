296630a33a70382db5271e180766a469ba38f0ae
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2023-11-23 08:38:06 +0530

    Update welcome.py

--- VipX/plugins/welcome.py
+++ VipX/plugins/welcome.py
@@ -20,7 +20,7 @@ async def join_watcher(_, message):
         
             count = await app.get_chat_members_count(chat.id)
 
-            msg msg = (
+            msg = (
                 f"**🌷𝐇ᴇʏ {message.from_user.mention} 𝐖ᴇʟᴄᴏᴍᴇ 𝐈ɴ 𝐀 𝐍ᴇᴡ 𝐆ʀᴏᴜᴘ🥳**\n\n"
                 f"**📝𝐂ʜᴀᴛ 𝐍ᴀᴍᴇ:** {message.chat.title}\n➖➖➖➖➖➖➖➖➖➖➖\n"
                 f"**🔐𝐂ʜᴀᴛ 𝐔.𝐍:** @{message.chat.username}\n➖➖➖➖➖➖➖➖➖➖➖\n"
